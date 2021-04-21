be21959ee58d019ed1c15e9fdd24d9b89f3ae751
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-21 09:22:18 +0200

    push

---
+++ main.js
@@ -0,0 +1,45 @@
+let temps = "pluie";
+
+switch (temps) {
+    case 'soleil':
+    console.log('mets ton plus beau maillot');
+    break;
+    case 'pluie':
+    console.log('its parapluie time');
+    break;
+    case 'tiède':
+    console.log('jsp mais prend ton abbonement de bus');
+    break;
+    case 'grêle':
+    console.log('un bob pour avoir le style et the proteger"');
+    break;
+    case 'tempête de neige':
+    console.log('des ski!');
+    break;
+    case 'tempête de sable':
+    console.log('topn sot');
+    break;
+    case 'venteux':
+    console.log('ton kite et ton fric');
+    break;
+    case 'gris':
+    console.log('noir');
+    break;
+    case 'triste':
+    console.log('un bloc note');
+    break;
+    case "à l'heure":
+    console.log('change de montre');
+    break;
+    case "caliente":
+    console.log('holla chica');
+    break;
+    case "plein":
+    console.log('passe en partiel');
+    break;
+    case "impartis":
+    console.log('dépêche toi');
+    break;
+    default:
+    console.log(`tout temps est bon pour faire du skate mon gars`);
+}
